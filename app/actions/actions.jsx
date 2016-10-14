f6c8e2331755b52a0e7284fbbb5407b5081ec78d
Zacck Osiemo <zacck@users.noreply.github.com>
2016-10-14 13:42:49 +0200

    Added dispatch for login and logout actions
    I Added dispatch for login and logout actions  because I got lost in lecture 145 when my app wouldnt redirect to the correct screens and my tests were broken. With this dispatch calls the app redirects to the correct page.

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -104,6 +104,7 @@ export var startLogin = () => {
   return (dispatch, getState) => {
     return firebase.auth().signInWithPopup(githubProvider).then((result) => {
       console.log('Auth worked!', result);
+      dispatch(login(result.user.uid));
     }, (error) => {
       console.log('Unable to auth', error);
     });
@@ -120,6 +121,7 @@ export var startLogout = () => {
   return (dispatch, getState) => {
     return firebase.auth().signOut().then(() => {
       console.log('Logged out!');
+      dispatch(logout());
     });
   };
 };
